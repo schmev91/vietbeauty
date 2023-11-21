ace801f5b07e33ae1c92d3656da528ffec97c004
Schmev Tran <trangiabaotran21@gmail.com>
2023-11-21 15:31:18 +0700

    seperate footer

--- views/home/index.php
+++ views/home/index.php
@@ -268,70 +268,8 @@
             </div>
         </section>
     </main>
-    <footer>
-        <div class="container-fluid mt-2 bg_footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-3 pt-3">
-                        <img src="views/asset/img/general/logo.png" alt="" style="padding-left: 30px;">
-                        <div class="theochungtoi">
-                            <h3 style="margin-top: 10px; color:#fff; font-weight: 300;font-size: 20px;">Theo dõi chúng tôi trên</h3>
-                            <ul class="list-mxh" style="display: flex; gap:20px; margin-top:10px; margin-left: 30px;">
-                                <li><i class="fa-brands fa-twitter" style="font-size: 25px;"></i></li>
-                                <li><i class="fa-brands fa-instagram" style="font-size: 25px;"></i></li>
-                                <li><i class="fa-brands fa-facebook-f" style="font-size: 25px;"></i></li>
-                                <li><i class="fa-brands fa-youtube" style="font-size: 25px;"></i></li>
-                            </ul>
-                        </div>
-                    </div>
 
-                    <div class="col-md-3 pt-4">
-                        <div class="vechungtoi">
-                            <h3 style="font-size: 25px; color:#fff;font-weight: 500;">Về Chúng Tôi</h3>
-                            <ul style="margin-top: 20px; display: flex; flex-direction: column; gap:10px">
-                                <li style="color:#fff">Đội nhóm</li>
-                                <li style="color:#fff">Chính Sách</li>
-                                <li style="color:#fff">Lịch Sử</li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="col-md-3 pt-4">
-                        <div class="hotro">
-                            <h3 style="font-size: 25px; color:#fff;font-weight: 500;">Hỗ trợ</h3>
-                            <ul style="margin-top: 20px; display: flex; flex-direction: column; gap:10px">
-                                <li style="color:#fff">Liên hệ</li>
-                                <li style="color:#fff">FAQ</li>
-                                <li style="color:#fff">CSKH</li>
-                                <li style="color:#fff">Điều khoản</li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="col-md-3 pt-4">
-                        <div class="hotline">
-                            <h3 style="font-size: 25px; color:#fff;font-weight: 500;">Hotline</h3>
-                            <ul style="margin-top: 20px; display: flex; flex-direction: column; gap:10px">
-                                <li style="color:#fff">0392706xxx</li>
-                                <li style="color:#fff">0937703xxx</li>
-                            </ul>
-                        </div>
-                        <div class="diachi">
-                            <h3 style="font-size: 25px; color:#fff;font-weight: 500; margin-top: 10px;">Địa Chỉ</h3>
-                            <ul style="margin-top: 10px; display: flex; flex-direction: column; gap:10px">
-                                <li style="color:#fff">Tòa nhà Innovation, CVPM Quang Trung, Quận 12, Thành Phố Hồ Chí Minh </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="container-fluid bg-ft-dark">
-            <div class="container p-2">
-                <span style="color:#fff; margin-left: 550px;">Bản quyền © 2023 viet beauty</span>
-            </div>
-        </div>
-    </footer>
+    <?php include_once "views/includes/footer.php" ?>
 </body>
 
 </html>
