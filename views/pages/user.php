21001a5aa843ca47d4ee9c8a7233c896dd575126
huune <trandaihuu4766@gmail.com>
2023-11-22 10:24:57 +0700

    trang user

---
+++ views/pages/user.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+    <link rel="stylesheet" href="views/asset/css/general.css">
+    <link rel="stylesheet" href="views/asset/css/user.css">
+</head>
+
+<body>
+
+    <?php include_once "views/includes/header.php" ?>
+
+    <main>
+      <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <span>Chào bạn</span>
+                <i class="fa-solid fa-circle-user"></i>
+            </div>
+        </div>
+      </div>
+    </main>
+
+    <?php include_once "views/includes/footer.php" ?>
+</body>
+
+</html>
